03ba006ab55e667fb24f7742cdeb7f947fae43ad
Ferdinando Ametrano <nando@users.sourceforge.net>
2003-04-22 12:27:11 +0000

    version number updated
    git-svn-id: https://svn.code.sf.net/p/xlw/code/trunk/xlw@220 948fe115-5b3c-0410-8233-88faca9dced8

--- xlw/EXCEL32_API.h
+++ xlw/EXCEL32_API.h
@@ -30,13 +30,13 @@
 */
 
 //! version hexadecimal number
-#define XLW_HEX_VERSION 0x010201a0
+#define XLW_HEX_VERSION 0x010203a0
 
 //! version string
 #ifdef XLW_DEBUG
-    #define XLW_VERSION "1.2.2f0-cvs-debug"
+    #define XLW_VERSION "1.2.3a0-cvs-debug"
 #else
-    #define XLW_VERSION "1.2.2f0-cvs"
+    #define XLW_VERSION "1.2.3a0-cvs"
 #endif
 
 //! global trace level (may be superseded locally by a greater value)
