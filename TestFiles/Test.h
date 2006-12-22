347ec9d38a545f6bc7f6ee9fbf144b166d1aaf2a
Mark Joshi <markjoshi@users.sourceforge.net>
2006-12-22 04:20:43 +0000

    added timings
    git-svn-id: https://svn.code.sf.net/p/xlw/code/trunk/xlw@297 948fe115-5b3c-0410-8233-88faca9dced8

--- TestFiles/Test.h
+++ TestFiles/Test.h
@@ -20,63 +20,76 @@ EchoShort(short x // number to be echoed
 	   );
 
 MyMatrix // echoes a matrix
+//<xlw:time
 EchoMat(const MyMatrix& Echoee // argument to be echoed
 		);
 
 
 MyArray // echoes an array
+//<xlw:time
 EchoArray(const MyArray& Echoee // argument to be echoed
 				  );
 
 
 CellMatrix // echoes a cell matrix
+//<xlw:time
 EchoCells(const CellMatrix& Echoee// argument to be echoed
 				   );
 
 double // computes the circumference of a circle
+//<xlw:time
 Circ(double Diameter //the circle's diameter
 			);
 
 
 std::string // Concatenates two strings
+//<xlw:time
 Concat(std::string str1 // first string
 	   , std::string str2 //second string
 	   );
  
 MyArray // computes mean and variance of a range
+//<xlw:time
 Stats(const MyArray& data // input for computation
 	  );
 
 
 
 std::string // says hello name
+//<xlw:time
 HelloWorldAgain(std::string name // name to be echoed
            );
 
 double // echoes an unsigned long 
+//<xlw:time
 EchoUL( unsigned long b  // number to echo
 	 );
 
 double // echoes an int 
+//<xlw:time
 EchoInt( int b  // number to echo
 	 );
 
 double // tests DoubleOrNothingType
+//<xlw:time
 EchoDoubleOrNothing( DoubleOrNothing x // value to specify
 					, double defaultValue // value to use if not specified
 					);
 
 CellMatrix // echoes arg list
+//<xlw:time
 EchoArgList( ArgumentList args  // arguments to echo
 	 );
 
 
 double // system clock
-//<xlw:volatile    
+//<xlw:volatile
+//<xlw:time
 SystemTime(DoubleOrNothing ticksPerSecond // number to divide by
 		   );
 
 double // evaluate pay--off
+//<xlw:time
 PayOffEvaluation(const Wrapper<PayOff>& PayOffTable // table for payoff
 	   , double Spot // point for evaluation
 			);
