87488db8751c7dcf7e3b26d13d84c54c116bed7b
Eric Ehlers <ericehlers@users.sourceforge.net>
2008-04-03 15:08:41 +0000

    Provide EXCEL_END* functions specific to each of the XlfOper/XlfOper4/XlfOper12 types.  Clean up the documentation of these macros.
    git-svn-id: https://svn.code.sf.net/p/xlw/code/trunk/xlw@514 948fe115-5b3c-0410-8233-88faca9dced8

--- include/xlw/macros.h
+++ include/xlw/macros.h
@@ -1,5 +1,6 @@
 /*
  Copyright (C) 1998, 1999, 2001, 2002, 2003, 2004 Jérôme Lecomte
+ Copyright (C) 2007, 2008 Eric Ehlers
 
  This file is part of XLW, a free-software/open-source C++ wrapper of the
  Excel C API - http://xlw.sourceforge.net/
@@ -37,39 +38,48 @@ Export macro that tells the compiler that the function is to be exported.
 */
 #define EXCEL_EXPORT __declspec(dllexport)
 
-
 //! Initialization macro
 /*!
-Macros to be inserted at the begining and end of *each* C function
-to be exported to excel.
+Use this macro at the beginning of each user defined function implemented in
+the addin.
 
-Frees the internal buffer if needed of XlfExcel used to return
-heap allocated data to Excel.
+If necessary, frees the internal buffer maintained by XlfExcel for heap memory
+that is returned to Excel.
 \sa XlfExcel
 */
 #define EXCEL_BEGIN XlfExcel::Instance().FreeMemory(); \
 try \
 {
 
-//! Clean up macro
-/*!
-Macro to be inserted at the end of *each* C function to be exported
-to excel.
+/*! \defgroup cleanup_macros Cleanup Macros
+Use a cleanup macro at the end of each user defined function implemented in the
+addin.  Choose the variation of EXCEL_END* which corresponds to the data type
+of the function's return value.
 
-Catch exception in C function calls
-Catching rules are as follow :
-- try first to catch excel exception that need the framework to return to excel
-asap (namely coerce, uncalculated cell, and abort). If catched, returns 0 that
-Excel will translate to \#NULL!.
-- catch runtime errors and send string to excel
-- catch strings and echo them
-- catch const char * and echo them
-- catch cell matrix and echo it, useful for complicated errors
-- catch all other kind of exception. Return \#VALUE! error.
+The cleanup macros catch any exception that was thrown during execution of the
+user defined addin function.
 
-You could easily add your own exception here. Note that it causes all your
-interface to recompile.
+The catch clause adheres to the following standards:
+- Test first for exceptions which require the framework to return to excel
+  immediately (namely coerce, uncalculated cell, and abort). If caught, returns
+  zero which is interpreted by Excel as \#NULL!.
+- Catch runtime errors and return string to Excel
+- Catch strings and echo them
+- Catch const char * values and echo them
+- Catch cell matrix and echo it, useful for complicated errors
+- Catch any exception of unknown type. Return \#VALUE! error.
+
+You can add your own exceptions here.  Note that changes to this file trigger a
+full recompilation of all addin code.
+
+\warning Excel-specific functions such as xlAutoOpen(), xlAddInManagerInfo(),
+etc. must not be wrapped in EXCEL_BEGIN/EXCEL_END.  EXCEL_END is capable of
+generating return values which Excel does not expect to receive from such
+functions.  EXCEL_BEGIN/EXCEL_END are for use only in user defined addin
+functions.
 */
+//@{
+//! Cleanup macro for function with return type XlfOper
 #define EXCEL_END \
 } catch (XlfException&) { \
     return 0; \
@@ -84,20 +94,41 @@ interface to recompile.
 } catch (...) { \
     return XlfOper::Error(xlerrValue); \
 }
-
-/* as example:
-#define EXCEL_END \
+//! Cleanup macro for function with return type XlfOper4
+#define EXCEL_END_4 \
 } catch (XlfException&) { \
     return 0; \
-} catch (yourException&) { \
-    exceptionHandling; \
+} catch (std::runtime_error& error){\
+    return XlfOper4(error.what());\
+} catch (std::string& error){\
+    return XlfOper4(error);\
+} catch (const char* error){\
+    return XlfOper4(error);\
+} catch (const CellMatrix& error){\
+    return XlfOper4(error);\
 } catch (...) { \
-    return XlfOper::Error(xlerrValue); \
+    return XlfOper4::Error(xlerrValue); \
 }
-*/
+//! Cleanup macro for function with return type XlfOper12
+#define EXCEL_END_12 \
+} catch (XlfException&) { \
+    return 0; \
+} catch (std::runtime_error& error){\
+    return XlfOper12(error.what());\
+} catch (std::string& error){\
+    return XlfOper12(error);\
+} catch (const char* error){\
+    return XlfOper12(error);\
+} catch (const CellMatrix& error){\
+    return XlfOper12(error);\
+} catch (...) { \
+    return XlfOper12::Error(xlerrValue); \
+}
+//@}
 
 #define __HERE__ __FILE__ "(" _MAKESTRING(__LINE__) "): "
 #define _MAKESTRING(a) __MAKESTRING(a)
 #define __MAKESTRING(a) #a
 
 #endif
+
