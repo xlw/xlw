3dd27c17a1ad4131acef431b434690071bf22e69
Narinder Claire <narinder-claire@users.sourceforge.net>
2009-05-22 12:17:13 +0000

    Still fixing more problems with dealing with directory names with spaces ! Such a pain.
    git-svn-id: https://svn.code.sf.net/p/xlw/code/branches/xlw4/xlw4@662 948fe115-5b3c-0410-8233-88faca9dced8

--- BatchBuild/BuildAllxlwTemplates.bat
+++ BatchBuild/BuildAllxlwTemplates.bat
@@ -5,8 +5,8 @@ make -f xlwTemplateMakefile DEV=gcc-make
 
 
 
-"C:\Program Files\Microsoft Visual Studio .NET 2003\Common7\IDE\devenv.exe"   C:\TEMP\xlwTemplateProjects\vc7\Template.sln  /rebuild Debug    /out xlwTemplate7Debug.log
-"C:\Program Files\Microsoft Visual Studio .NET 2003\Common7\IDE\devenv.exe"   C:\TEMP\xlwTemplateProjects\vc7\Template.sln  /rebuild Release  /out xlwTemplate7Release.log
-"C:\Program Files\Microsoft Visual Studio 8\VC\vcpackages\vcbuild.exe" /rebuild    C:\TEMP\xlwTemplateProjects\vc8\Template.sln  >> xlwTemplate8.log 2>&1 
-"C:\Program Files\Microsoft Visual Studio 9.0\VC\vcpackages\vcbuild.exe" /rebuild  C:\TEMP\xlwTemplateProjects\vc9\Template.sln  >> xlwTemplate9.log 2>&1 
+"C:\Program Files\Microsoft Visual Studio .NET 2003\Common7\IDE\devenv.exe"   "C:\TEMP\xlwTemplate Projects\vc7\Template.sln"  /rebuild Debug    /out xlwTemplate7Debug.log
+"C:\Program Files\Microsoft Visual Studio .NET 2003\Common7\IDE\devenv.exe"   "C:\TEMP\xlwTemplate Projects\vc7\Template.sln"  /rebuild Release  /out xlwTemplate7Release.log
+"C:\Program Files\Microsoft Visual Studio 8\VC\vcpackages\vcbuild.exe" /rebuild    "C:\TEMP\xlwTemplate Projects\vc8\Template.sln"  >> xlwTemplate8.log 2>&1 
+"C:\Program Files\Microsoft Visual Studio 9.0\VC\vcpackages\vcbuild.exe" /rebuild  "C:\TEMP\xlwTemplate Projects\vc9\Template.sln"  >> xlwTemplate9.log 2>&1 
 
--- BatchBuild/prepareDotNetTemplateProject.py
+++ BatchBuild/prepareDotNetTemplateProject.py
@@ -4,5 +4,5 @@ import os
 import shutil
 
 
-shutil.rmtree("C:/Temp/xlwDotNetTemplate_Projects",True)
-shutil.copytree('../xlwDotNet/Template_Projects','C:/Temp/xlwDotNetTemplate_Projects',ignore=shutil.ignore_patterns('*.xls', '.svn'))
+shutil.rmtree("C:/Temp/xlwDotNetTemplate Projects",True)
+shutil.copytree('../xlwDotNet/Template_Projects','C:/Temp/xlwDotNetTemplate Projects',ignore=shutil.ignore_patterns('*.xls', '.svn'))
