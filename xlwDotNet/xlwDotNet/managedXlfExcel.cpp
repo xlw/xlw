3b6db5d9d868d3f0ea09fc00daec393651b2ed9e
adcockj <adcockj@users.sourceforge.net>
2011-10-12 11:48:49 +0000

    Add new .NET wrapper for XlfExcel and expose the RTD function
    git-svn-id: https://svn.code.sf.net/p/xlw/code/trunk/xlw@1320 948fe115-5b3c-0410-8233-88faca9dced8

---
+++ xlwDotNet/xlwDotNet/managedXlfExcel.cpp
@@ -0,0 +1,53 @@
+/*
+ Copyright (C) 2011 John Adcock
+
+ This file is part of XLWDOTNET, a free-software/open-source C# wrapper of the
+ Excel C API - http://xlw.sourceforge.net/
+
+ XLWDOTNET is part of XLW, a free-software/open-source C++ wrapper of the
+ Excel C API - http://xlw.sourceforge.net/
+
+ XLW is free software: you can redistribute it and/or modify it under the
+ terms of the XLW license.  You should have received a copy of the
+ license along with this program; if not, please email xlw-users@lists.sf.net
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+*/
+using namespace System;
+#include<xlw/xlw.h>
+#include"managedCellMatrix.h"
+
+std::string CLR2CPP(String^ clrString);
+
+namespace xlwDotNet
+{
+    public ref class XlwExcel
+    {
+
+    public:
+        XlwExcel()
+        {
+        }
+
+
+        static property int version
+        {
+            int get()
+            {
+                return xlw::XlfExcel::Instance().excelVersion();
+            }
+        }
+
+        static xlwTypes::CellMatrix^ RTD(String^ rtdServer, Int32 topicId)
+        {
+            // make sure everything is set up inside the library
+            xlw::XlfExcel::Instance();
+            xlw::XlfOper resultOper;
+            xlw::XlfExcel::Instance().Call(xlfRtd, resultOper, 3, xlw::XlfOper(CLR2CPP(rtdServer)), xlw::XlfOper(), xlw::XlfOper(topicId));
+            xlw::CellMatrix* resultMatrix(new xlw::CellMatrix(resultOper.AsCellMatrix()));
+             return gcnew xlwTypes::CellMatrix((IntPtr)resultMatrix, true);
+        }
+    };
+}
