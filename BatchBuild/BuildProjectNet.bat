3a67b08c03a9266332b936b1f350a31ac5e88974
Mark Owen <markowen@users.sourceforge.net>
2011-11-16 15:23:24 +0000

    Fixing broken build from commit #1343. Need to specify platform x86 for a .NET project, and Win32 for a CPP project.
    git-svn-id: https://svn.code.sf.net/p/xlw/code/trunk/xlw@1344 948fe115-5b3c-0410-8233-88faca9dced8

--- BatchBuild/BuildProjectNet.bat
+++ BatchBuild/BuildProjectNet.bat
@@ -1,9 +1,9 @@
 @SETLOCAL
 @if exist "%~1\VS8" for %%f in ("%~1\VS8\*.sln") do CALL BuildVS8NetProject.bat "%%f" %2
 @if exist "%~1\VS9" for %%f in ("%~1\VS9\*.sln") do CALL BuildVS9NetProject.bat "%%f" %2
-@if exist "%~1\VS10" for %%f in ("%~1\VS10\*.sln") do CALL BuildVS10Project.bat "%%f" %2
+@if exist "%~1\VS10" for %%f in ("%~1\VS10\*.sln") do CALL BuildVS10Project.bat "%%f" %2 NET
 @if exist "%~1\VS8_PRO" for %%f in ("%~1\VS8_PRO\*.sln") do CALL BuildVS8NetProject.bat "%%f" %2
 @if exist "%~1\VS9_PRO" for %%f in ("%~1\VS9_PRO\*.sln") do CALL BuildVS9NetProject.bat "%%f" %2
-@if exist "%~1\VS10_PRO" for %%f in ("%~1\VS10_PRO\*.sln") do CALL BuildVS10Project.bat "%%f" %2
+@if exist "%~1\VS10_PRO" for %%f in ("%~1\VS10_PRO\*.sln") do CALL BuildVS10Project.bat "%%f" %2 NET
 @if exist "%~1\VB2005" for %%f in ("%~1\VB2005\*.sln") do CALL BuildVS8NetProject.bat "%%f" %2
 @if exist "%~1\VB2008" for %%f in ("%~1\VB2008\*.sln") do CALL BuildVS9NetProject.bat "%%f" %2
