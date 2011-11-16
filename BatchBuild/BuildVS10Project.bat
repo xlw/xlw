f562a8f2e6094212068a9256b8f9d75b34412d65
Mark Owen <markowen@users.sourceforge.net>
2011-11-16 11:51:12 +0000

    Explicit choice of x86 platform in VS10 batch build. [On some systems, the environment variable PLATFORM can take non-standard values.]
    git-svn-id: https://svn.code.sf.net/p/xlw/code/trunk/xlw@1343 948fe115-5b3c-0410-8233-88faca9dced8

--- BatchBuild/BuildVS10Project.bat
+++ BatchBuild/BuildVS10Project.bat
@@ -3,8 +3,8 @@
 SET DevEnvDir=%VS10DEVENVDIR%
 SET vcbuildtoolpath=%VS10TOOLS%
 SET PATH=%VS10BIN%;%OLDPATH%
-%NET4BUILD% /p:Configuration=Debug /t:rebuild  %1  >> "%LOG_ROOT%x86-Debug.log" 2>&1 
-%NET4BUILD% /p:Configuration=Release /t:rebuild  %1  >> "%LOG_ROOT%x86-Release.log" 2>&1 
+%NET4BUILD% /p:Configuration=Debug:Platform=x86 /t:rebuild  %1  >> "%LOG_ROOT%x86-Debug.log" 2>&1 
+%NET4BUILD% /p:Configuration=Release:Platform=x86 /t:rebuild  %1  >> "%LOG_ROOT%x86-Release.log" 2>&1 
 %NET4BUILD% /p:Configuration=Debug;Platform=x64 /t:rebuild  %1  >> "%LOG_ROOT%x64-Debug.log" 2>&1 
 %NET4BUILD% /p:Configuration=Release;Platform=x64 /t:rebuild  %1  >> "%LOG_ROOT%x64-Release.log" 2>&1 
 @ENDLOCAL
