8e867393ff79e5eaa858c39c92ce57067794383a
Mark Owen <markowen@users.sourceforge.net>
2011-11-17 18:50:32 +0000

    Preparation of dotNetTemplates should not involve copying binaries which have been built in the xlw trunk into Temp folde, because this can cause build errors.
    git-svn-id: https://svn.code.sf.net/p/xlw/code/trunk/xlw@1348 948fe115-5b3c-0410-8233-88faca9dced8

--- BatchBuild/prepareDotNetTemplateProject.py
+++ BatchBuild/prepareDotNetTemplateProject.py
@@ -5,4 +5,4 @@ import shutil
 
 
 shutil.rmtree("C:/Temp/xlwDotNetTemplate Projects",True)
-shutil.copytree('../xlwDotNet/Template_Projects','C:/Temp/xlwDotNetTemplate Projects',ignore=shutil.ignore_patterns('*.xls', '.svn'))
+shutil.copytree('../xlwDotNet/Template_Projects','C:/Temp/xlwDotNetTemplate Projects',ignore=shutil.ignore_patterns('Debug','Release','x64_Debug','x64_Release','bin','obj','*.xls', '.svn'))
