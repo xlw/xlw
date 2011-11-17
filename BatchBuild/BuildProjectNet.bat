a618236008ed2f971749ef8d1456dfe1cedbc9a5
Mark Owen <markowen@users.sourceforge.net>
2011-11-17 23:48:48 +0000

    Removed unnecessary SETLOCAL.
    git-svn-id: https://svn.code.sf.net/p/xlw/code/trunk/xlw@1351 948fe115-5b3c-0410-8233-88faca9dced8

--- BatchBuild/BuildProjectNet.bat
+++ BatchBuild/BuildProjectNet.bat
@@ -1,4 +1,3 @@
-@SETLOCAL
 @if exist "%~1\VS8" for %%f in ("%~1\VS8\*.sln") do CALL BuildVS8NetProject.bat "%%f" %2
 @if exist "%~1\VS9" for %%f in ("%~1\VS9\*.sln") do CALL BuildVS9NetProject.bat "%%f" %2
 @if exist "%~1\VS10" for %%f in ("%~1\VS10\*.sln") do CALL BuildVS10Project.bat "%%f" %2 NET
