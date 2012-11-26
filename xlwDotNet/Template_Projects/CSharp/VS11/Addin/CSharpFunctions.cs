cba367cd6ccb5c5d2bb535a65eeb96d5718bfe04
Narinder Claire <narinder-claire@users.sourceforge.net>
2012-11-26 08:58:02 +0000

    Update XLW for Visual Studio 2012. the xlwDotnNet stuff builds .. the template project works fine too. BUT the Start Here Example build without errors but nothing shows up in EXcel .... not great but not a show stopper
    git-svn-id: https://svn.code.sf.net/p/xlw/code/trunk/xlw@1413 948fe115-5b3c-0410-8233-88faca9dced8

---
+++ xlwDotNet/Template_Projects/CSharp/VS11/Addin/CSharpFunctions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using xlwDotNet;
+using xlwDotNet.xlwTypes;
+
+
+namespace Example
+{
+    public class Class1
+    {
+     
+        [ExcelExport("Hello World")]
+        public static String HelloWorld()
+        {
+            return "Hello World!";
+        }
+
+        
+    }
+}
+
