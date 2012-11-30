e8e761ef437a8d28429ac19771c2c2e1d12097d7
Narinder Claire <narinder-claire@users.sourceforge.net>
2012-11-30 00:08:12 +0000

    Hybrid Template
    git-svn-id: https://svn.code.sf.net/p/xlw/code/trunk/xlw@1429 948fe115-5b3c-0410-8233-88faca9dced8

---
+++ xlwDotNet/Template_Projects/Hybrid_Cpp_CSharp_XLLs/VS11_PRO/cplusplus_source/Test.cpp
@@ -0,0 +1,13 @@
+#include <xlw/MyContainers.h>
+#include <xlw/CellMatrix.h>
+#include <xlw/DoubleOrNothing.h>
+#include <xlw/ArgList.h>
+#include <string>
+
+using namespace xlw;
+
+std::string  // C++ Function
+CPlusPlusFunction()
+{
+    return "Hello from the Native C++ World !";
+}
