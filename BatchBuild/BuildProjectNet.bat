b4a9db9890463c51dc5d0c00fe35fd86d81163e4
adcockj <adcockj@users.sourceforge.net>
2014-07-16 05:55:01 +0000

    Call correct version of build batch for .NET in btach build
    git-svn-id: https://svn.code.sf.net/p/xlw/code/trunk/xlw@1471 948fe115-5b3c-0410-8233-88faca9dced8

--- BatchBuild/BuildProjectNet.bat
+++ BatchBuild/BuildProjectNet.bat
@@ -1,8 +1,8 @@
 @if exist "%~1\VS8" for %%f in ("%~1\VS8\*.sln") do CALL BuildVS8NetProject.bat "%%f" %2
 @if exist "%~1\VS9" for %%f in ("%~1\VS9\*.sln") do CALL BuildVS9NetProject.bat "%%f" %2
 @if exist "%~1\VS10" for %%f in ("%~1\VS10\*.sln") do CALL BuildVS10Project.bat "%%f" %2 NET
-@if exist "%~1\VS11" for %%f in ("%~1\VS11\*.sln") do CALL BuildVS10Project.bat "%%f" %2 NET
-@if exist "%~1\VS12" for %%f in ("%~1\VS12\*.sln") do CALL BuildVS10Project.bat "%%f" %2 NET
+@if exist "%~1\VS11" for %%f in ("%~1\VS11\*.sln") do CALL BuildVS11Project.bat "%%f" %2 NET
+@if exist "%~1\VS12" for %%f in ("%~1\VS12\*.sln") do CALL BuildVS12Project.bat "%%f" %2 NET
 @if exist "%~1\VS8_PRO" for %%f in ("%~1\VS8_PRO\*.sln") do CALL BuildVS8NetProject.bat "%%f" %2
 @if exist "%~1\VS9_PRO" for %%f in ("%~1\VS9_PRO\*.sln") do CALL BuildVS9NetProject.bat "%%f" %2
 @if exist "%~1\VS10_PRO" for %%f in ("%~1\VS10_PRO\*.sln") do CALL BuildVS10Project.bat "%%f" %2 NET
