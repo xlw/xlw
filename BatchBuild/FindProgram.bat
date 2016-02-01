36c1b5ded1dc2d40eb5de8e3b03bcc000b8eb16f
adcockj <adcockj@users.sourceforge.net>
2016-02-01 20:08:26 +0000

    Allow for 32 bit java runtime to find 64  bit programs
    git-svn-id: https://svn.code.sf.net/p/xlw/code/branches/Dev6/xlw@1495 948fe115-5b3c-0410-8233-88faca9dced8

--- BatchBuild/FindProgram.bat
+++ BatchBuild/FindProgram.bat
@@ -2,6 +2,7 @@
 @SET %2=
 @CALL CheckFile.bat "%ProgramFiles%\%~1" %2 %3
 @CALL CheckFile.bat "%ProgramFiles(x86)%\%~1" %2 %3
+@CALL CheckFile.bat "%ProgramFilesW6432%\%~1" %2 %3
 @IF not defined %2 ECHO Error: Can't Find %2 & exit /b 1
 @ECHO Found %2
 @EXIT /b 0
