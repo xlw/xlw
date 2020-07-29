7040e384cd3c37a739bb22eae9cc8472fd7b0500
Ninds <narinder.claire@gmail.com>
2020-07-29 19:34:53 +0100

    DEV : Add some initial source files to be added by the Nuget package

---
+++ xlw/nuget_content/cppinterface.h
@@ -0,0 +1,23 @@
+//
+//
+//                                                                    Test.h
+//
+
+#ifndef TEST_H
+#define TEST_H
+
+#include <xlw/MyContainers.h>
+#include <xlw/CellMatrix.h>
+#include <xlw/DoubleOrNothing.h>
+#include <xlw/ArgList.h>  
+    
+using namespace xlw;
+
+//<xlw:libraryname=MyTestLibrary
+
+
+short // echoes a short
+EchoShort(short x // number to be echoed
+       );
+
+#endif
---
+++ xlw/nuget_content/source.cpp
@@ -0,0 +1,12 @@
+
+#include"cppinterface.h"
+#pragma warning (disable : 4996)
+
+
+short // echoes a short
+EchoShort(short x // number to be echoed
+           )
+{
+    return x;
+}
+
