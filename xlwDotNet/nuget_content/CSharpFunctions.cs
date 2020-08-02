3410d8e6cfd001aec68e9d9703b5a5033bf7ea74
Ninds <narinder.claire@gmail.com>
2020-08-02 21:04:00 +0100

    DEV : NugetPackage for .NET :-)

---
+++ xlwDotNet/nuget_content/CSharpFunctions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using xlwDotNet;
+using xlwDotNet.xlwTypes;
+
+
+namespace Example
+{
+    public class Class1
+    {
+     
+        [ExcelExport("Hello World")]
+        public static String HelloWorld()
+        {
+            return "Hello World!";
+        }
+
+
+        [ExcelExport("Bye World")]
+        public static String byeWorld()
+        {
+            return "Bye World!";
+        }
+
+        [ExcelExport("Get's .NET Version")]
+        public static String dotnetversion()
+        {
+            return System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription;
+        }
+
+
+    }
+}
+
